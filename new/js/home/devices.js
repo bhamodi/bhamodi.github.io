24c72a63f9377e9693778195786edf97ff417115
Baraa <bhamodi@uwaterloo.ca>
2014-10-26 00:05:22 -0700

    Made audio button responsive. Cleaned up JS.

--- new/js/home/devices.js
+++ new/js/home/devices.js
@@ -1,7 +1,5 @@
 "use strict";
-
 $(document).ready(function() {
-	// /Android|AppleWebKit|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
 	if(/Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 		$('#home').css('display','none');
 		$('#wrapper_mbYTP_bgndVideo').css('display','none');
--- new/js/home/video/jquery.YTPlayer.js
+++ new/js/home/video/jquery.YTPlayer.js
@@ -2,57 +2,20 @@ $(document).ready(function() {
 	var onMobile = false;
 	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) onMobile = true;
 	
-	if (( onMobile === false )) {
+	if (onMobile === false) {
 			$(".player").mb_YTPlayer();
 	} else {
-		/* as a fallback we add a special class to the header which displays a poster image */ 
-		$('#home').addClass('video-section');
 		/* hide player */
 		$(".player").hide();
 		$("#video-volume").hide();
 	}
 });
 
-/*
- * ******************************************************************************
- *  jquery.mb.components
- *  file: jquery.mb.YTPlayer.js
- *
- *  Copyright (c) 2001-2013. Matteo Bicocchi (Pupunzi);
- *  Open lab srl, Firenze - Italy
- *  email: matteo@open-lab.com
- *  site: 	http://pupunzi.com
- *  blog:	http://pupunzi.open-lab.com
- * 	http://open-lab.com
- *
- *  Licences: MIT, GPL
- *  http://www.opensource.org/licenses/mit-license.php
- *  http://www.gnu.org/licenses/gpl.html
- *
- *  last modified: 30/08/13 23.31
- *  *****************************************************************************
- */
-
 /*Browser detection patch*/
 (function(){if(!(8>jQuery.fn.jquery.split(".")[1])){jQuery.browser={};jQuery.browser.mozilla=!1;jQuery.browser.webkit=!1;jQuery.browser.opera=!1;jQuery.browser.msie=!1;var a=navigator.userAgent;jQuery.browser.name=navigator.appName;jQuery.browser.fullVersion=""+parseFloat(navigator.appVersion);jQuery.browser.majorVersion=parseInt(navigator.appVersion,10);var c,b;if(-1!=(b=a.indexOf("Opera"))){if(jQuery.browser.opera=!0,jQuery.browser.name="Opera",jQuery.browser.fullVersion=a.substring(b+6),-1!=(b= a.indexOf("Version")))jQuery.browser.fullVersion=a.substring(b+8)}else if(-1!=(b=a.indexOf("MSIE")))jQuery.browser.msie=!0,jQuery.browser.name="Microsoft Internet Explorer",jQuery.browser.fullVersion=a.substring(b+5);else if(-1!=(b=a.indexOf("Chrome")))jQuery.browser.webkit=!0,jQuery.browser.name="Chrome",jQuery.browser.fullVersion=a.substring(b+7);else if(-1!=(b=a.indexOf("Safari"))){if(jQuery.browser.webkit=!0,jQuery.browser.name="Safari",jQuery.browser.fullVersion=a.substring(b+7),-1!=(b=a.indexOf("Version")))jQuery.browser.fullVersion= a.substring(b+8)}else if(-1!=(b=a.indexOf("Firefox")))jQuery.browser.mozilla=!0,jQuery.browser.name="Firefox",jQuery.browser.fullVersion=a.substring(b+8);else if((c=a.lastIndexOf(" ")+1)<(b=a.lastIndexOf("/")))jQuery.browser.name=a.substring(c,b),jQuery.browser.fullVersion=a.substring(b+1),jQuery.browser.name.toLowerCase()==jQuery.browser.name.toUpperCase()&&(jQuery.browser.name=navigator.appName);if(-1!=(a=jQuery.browser.fullVersion.indexOf(";")))jQuery.browser.fullVersion=jQuery.browser.fullVersion.substring(0, a);if(-1!=(a=jQuery.browser.fullVersion.indexOf(" ")))jQuery.browser.fullVersion=jQuery.browser.fullVersion.substring(0,a);jQuery.browser.majorVersion=parseInt(""+jQuery.browser.fullVersion,10);isNaN(jQuery.browser.majorVersion)&&(jQuery.browser.fullVersion=""+parseFloat(navigator.appVersion),jQuery.browser.majorVersion=parseInt(navigator.appVersion,10));jQuery.browser.version=jQuery.browser.majorVersion}})(jQuery);
-
-/*******************************************************************************
- * jQuery.mb.components: jquery.mb.CSSAnimate
- ******************************************************************************/
-
 jQuery.fn.CSSAnimate=function(a,b,k,l,f){return this.each(function(){var c=jQuery(this);if(0!==c.length&&a){"function"==typeof b&&(f=b,b=jQuery.fx.speeds._default);"function"==typeof k&&(f=k,k=0);"function"==typeof l&&(f=l,l="cubic-bezier(0.65,0.03,0.36,0.72)");if("string"==typeof b)for(var j in jQuery.fx.speeds)if(b==j){b=jQuery.fx.speeds[j];break}else b=null;if(jQuery.support.transition){var e="",h="transitionEnd";jQuery.browser.webkit?(e="-webkit-",h="webkitTransitionEnd"):jQuery.browser.mozilla? (e="-moz-",h="transitionend"):jQuery.browser.opera?(e="-o-",h="otransitionend"):jQuery.browser.msie&&(e="-ms-",h="msTransitionEnd");j=[];for(d in a){var g=d;"transform"===g&&(g=e+"transform",a[g]=a[d],delete a[d]);"transform-origin"===g&&(g=e+"transform-origin",a[g]=a[d],delete a[d]);j.push(g);c.css(g)||c.css(g,0)}d=j.join(",");c.css(e+"transition-property",d);c.css(e+"transition-duration",b+"ms");c.css(e+"transition-delay",k+"ms");c.css(e+"transition-timing-function",l);c.css(e+"backface-visibility", "hidden");setTimeout(function(){c.css(a)},0);setTimeout(function(){c.called||!f?c.called=!1:f()},b+20);c.on(h,function(a){c.off(h);c.css(e+"transition","");a.stopPropagation();"function"==typeof f&&(c.called=!0,f());return!1})}else{for(var d in a)"transform"===d&&delete a[d],"transform-origin"===d&&delete a[d],"auto"===a[d]&&delete a[d];if(!f||"string"===typeof f)f="linear";c.animate(a,b,f)}}})}; jQuery.fn.CSSAnimateStop=function(){var a="",b="transitionEnd";jQuery.browser.webkit?(a="-webkit-",b="webkitTransitionEnd"):jQuery.browser.mozilla?(a="-moz-",b="transitionend"):jQuery.browser.opera?(a="-o-",b="otransitionend"):jQuery.browser.msie&&(a="-ms-",b="msTransitionEnd");jQuery(this).css(a+"transition","");jQuery(this).off(b)}; jQuery.support.transition=function(){var a=(document.body||document.documentElement).style;return void 0!==a.transition||void 0!==a.WebkitTransition||void 0!==a.MozTransition||void 0!==a.MsTransition||void 0!==a.OTransition}();
-
-/*
- * Metadata - jQuery plugin for parsing metadata from elements
- * Copyright (c) 2006 John Resig, Yehuda Katz, Jörn Zaefferer, Paul McLanahan
- * Dual licensed under the MIT and GPL licenses:
- *   http://www.opensource.org/licenses/mit-license.php
- *   http://www.gnu.org/licenses/gpl.html
- */
-
 (function(c){c.extend({metadata:{defaults:{type:"class",name:"metadata",cre:/({.*})/,single:"metadata"},setType:function(b,c){this.defaults.type=b;this.defaults.name=c},get:function(b,f){var d=c.extend({},this.defaults,f);d.single.length||(d.single="metadata");var a=c.data(b,d.single);if(a)return a;a="{}";if("class"==d.type){var e=d.cre.exec(b.className);e&&(a=e[1])}else if("elem"==d.type){if(!b.getElementsByTagName)return;e=b.getElementsByTagName(d.name);e.length&&(a=c.trim(e[0].innerHTML))}else void 0!= b.getAttribute&&(e=b.getAttribute(d.name))&&(a=e);0>a.indexOf("{")&&(a="{"+a+"}");a=eval("("+a+")");c.data(b,d.single,a);return a}}});c.fn.metadata=function(b){return c.metadata.get(this[0],b)}})(jQuery);
 
-/***************************************************************************************/
 if (typeof ytp != "object") ytp ={};
 
 String.prototype.getVideoID=function() {
@@ -78,9 +41,9 @@ function onYouTubePlayerAPIReady() {
 
 (function (jQuery) {
 	jQuery.mbYTPlayer = {
-		name           : "jquery.mb.YTPlayer",
+		name           : "jquery.YTPlayer",
 		version        : "2.5.7",
-		author         : "Matteo Bicocchi",
+		author         : "Baraa Hamodi",
 		defaults       : {
 			containment            : "body",
 			ratio                  : "16/9",
@@ -163,7 +126,6 @@ function onYouTubePlayerAPIReady() {
 				if (!YTPlayer.opt.videoURL) {
 					YTPlayer.opt.videoURL = $YTPlayer.attr("href");
 				}
-
 				/************************************************************/
 
 				var playerID = "mbYTP_" + YTPlayer.id;
@@ -277,9 +239,7 @@ function onYouTubePlayerAPIReady() {
 					}
 
 					YTPlayer.opt.isInit = true;
-
 					YTPlayer.opt.vol = YTPlayer.opt.vol ? YTPlayer.opt.vol : 100;
-
 					jQuery.mbYTPlayer.getDataFromFeed(YTPlayer.videoID, YTPlayer);
 
 					jQuery(document).on("getVideoInfo_" + YTPlayer.opt.id, function() {
@@ -345,7 +305,6 @@ function onYouTubePlayerAPIReady() {
 												}
 											}
 										}, 1);
-
 									} else {
 										$YTPlayer.playYTP();
 										YTPlayer.player.setVolume(YTPlayer.opt.vol);
@@ -384,7 +343,6 @@ function onYouTubePlayerAPIReady() {
 
 									var playerBox = jQuery(YTPlayer.playerEl);
 									var controls = jQuery("#controlBar_" + YTPlayer.id);
-
 									var data = YTPlayer.opt;
 
 									if (state == 0) { // end
@@ -593,7 +551,6 @@ function onYouTubePlayerAPIReady() {
 				jQuery(YTPlayer).one("YTPStart", function(){
 					jQuery(YTPlayer.playerEl).CSSAnimate({opacity:1},2000);
 				});
-
 			},timer)
 
 			if (YTPlayer.opt.mute) {
@@ -638,7 +595,6 @@ function onYouTubePlayerAPIReady() {
 
 		fullscreen: function(real) {
 			var YTPlayer = this.get(0);
-
 			var controls = jQuery("#controlBar_" + YTPlayer.id);
 			var fullScreenBtn = controls.find(".mb_OnlyYT");
 			var videoWrapper = jQuery(YTPlayer.wrapper);
@@ -797,10 +753,8 @@ function onYouTubePlayerAPIReady() {
 
 		unmuteYTPVolume: function() {
 			var YTPlayer = this.get(0);
-
 			YTPlayer.player.unMute();
 			YTPlayer.player.setVolume(YTPlayer.opt.vol);
-
 			var controls = jQuery("#controlBar_" + YTPlayer.id);
 			var muteBtn = controls.find(".mb_YTVPMuteUnmute");
 			muteBtn.html(jQuery.mbYTPlayer.controls.mute);
