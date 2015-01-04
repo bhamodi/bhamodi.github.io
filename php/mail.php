1cf3a092a567504519d1f6fe8fef44b8085bbfd9
Baraa <bhamodi@uwaterloo.ca>
2015-01-04 14:47:15 -0500

    Update email backend template.

--- php/mail.php
+++ php/mail.php
@@ -1,6 +1,6 @@
 <?php
-// check if fields passed are empty
-if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) || !FILTER_VAR($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+// Check if the input fields are empty.
+if (empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) || !FILTER_VAR($_POST['email'], FILTER_VALIDATE_EMAIL)) {
 	echo "No arguments Provided!";
 	return false;
 }
@@ -9,13 +9,16 @@ $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
 
-// create email body and send it
-$to = "bhamodi@uwaterloo.ca"; // my email
-$email_subject = "Email submitted by:  $name";
-$email_body = "You have received a new message. \n".
-				  "Here are the details:\n\nName: $name \n".
-				  "Email: $email_address\nMessage:\n$message";
-$headers = "From: $email_address\n";
+// Create email template.
+$to = "bhamodi@uwaterloo.ca";
+$email_subject = "Email submitted by: $name";
+$email_body =
+    "You have received a new message.\n".
+    "Here are the details:\n\n".
+    "Name: $name\n".
+	"Email: $email_address\n".
+    "Message: $message";
 $headers = "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
+
+mail($to, $email_subject, $email_body, $headers);
 return true;
