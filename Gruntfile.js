90d101714510a1b01333669d6b0becea62c7550a
Baraa Hamodi <bhamodi@uwaterloo.ca>
2015-06-06 23:31:52 -0700

    Configured grunt and uglifyjs to make production ready compiled and minified js files.

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		uglify: {
+			dist: {
+				files: {
+					'js/dist/main.min.js': 'js/*.js',
+					'js/dist/home.min.js': 'js/home/*.js'
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.registerTask('default', ['uglify']);
+}
