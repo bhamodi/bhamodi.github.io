9a245e50dc05dafe92c30e72be0a5b4a4b50ce79
Baraa Hamodi <baraa@optimizely.com>
2015-07-17 14:21:25 -0700

    Updated scrollTop animation logic.

--- js/home/home.js
+++ js/home/home.js
@@ -16,16 +16,84 @@ $(document).ready(function() {
   /* Menu Headers */
   $('.header').sticky({topSpacing: 0});
   $('a[href*=#]').click(function() {
-    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-      var $target = $(this.hash);
-      $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
-      if ($target.length) {
-        var targetOffset = $target.offset().top;
-        $('html,body').animate({scrollTop: targetOffset - 75}, 2000);
-        return false;
-      }
+    var $target = $(this.hash);
+    $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+    if ($target.length) {
+      var targetOffset = $target.offset().top;
+      $('html,body').animate({scrollTop: targetOffset - 75}, 2000);
+      return false;
     }
   });
+
+  /* Dynamic Window Ajax Portfolio Content */
+  // Open project and set actual to active project.
+  var $actual = null;
+  $('.portfolio-element').click(function() {
+    openProject($(this).attr('id'));
+    $actual = $(this);
+  });
+
+  // Slide up project when a filter is applied.
+  $('.folio-btn').click(function() {
+    $('.project-window').slideUp('slow');
+  });
+
+  function openProject(projectName) {
+    $.ajax({
+      url: projectName,
+      success: function(data) {
+        $('.project-content').html(data);
+        $('.project-window').fadeIn('slow');
+        closeProject();
+        changeProject();
+        $('html, body').animate({ scrollTop: $('#portfolio').offset().top }, 500);
+      }
+    });
+  }
+
+  function closeProject() {
+    $('.close').click(function() {
+      $('.project-window').slideUp('slow');
+      $('html, body').animate({ scrollTop: $('#projects').offset().top }, 500);
+    });
+  }
+
+  function nextProject() {
+    if ($actual.next().hasClass('final')) {
+      $actual = $($('.portfolioBase').next());
+    } else {
+      $actual = $($actual.next());
+    }
+    if ($actual.hasClass('isotope-hidden')) {
+      nextProject();
+    } else {
+      openProject($actual.attr('id'));
+    }
+  }
+
+  function prevProject() {
+    if ($actual.prev().hasClass('portfolioBase')) {
+      $actual = $($('.final').prev());
+    } else {
+      $actual = $($actual.prev());
+    }
+    if ($actual.hasClass('isotope-hidden')) {
+      prevProject();
+    } else {
+      openProject($actual.attr('id'));
+    }
+  }
+
+  function changeProject() {
+    $('.next-button').click(function() {
+      nextProject();
+      $('html, body').animate({ scrollTop: $('#project-show').offset().top }, 500);
+    });
+    $('.prev-button').click(function() {
+      prevProject();
+      $('html, body').animate({ scrollTop: $('#project-show').offset().top }, 500);
+    });
+  }
 
   /* Services RollOver Info */
   function loadServices() {
