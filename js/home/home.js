db2ace0bc71cea3d20e7df948df6a826a0a493cc
Baraa Hamodi <bhamodi@uwaterloo.ca>
2018-04-06 16:03:56 -0400

    Update graduation date to convocation date.

--- js/home/home.js
+++ js/home/home.js
@@ -449,7 +449,7 @@ $(document).ready(function() {
 
   /* Dynamic Days Left Calculator */
   function calculateDaysToGraduation() {
-    var gradDay = new Date('2018-04-23');
+    var gradDay = new Date('2018-06-16');
     var today = new Date();
     var millisecondsPerDay = 24 * 60 * 60 * 1000;
     var utcToday = Date.UTC(today.getFullYear(), today.getMonth(), today.getDate());
